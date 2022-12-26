a7bfb2c5fec2ca1653ef717c47b18736e681a75c
rwillett9 <rjw4321@gmail.com>
2022-12-25 20:41:28 -0500

    reorganized frontend file structure

--- pi_dashboard/app/http/web/src/index.js
+++ pi_dashboard/app/http/web/src/index.js
@@ -2,15 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css';
-import App from './App/App';
 import reportWebVitals from './reportWebVitals';
+
+import App from './Components/App/App';
+import Home from './Components/Home/Home'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<App />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/test' element={<App />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
